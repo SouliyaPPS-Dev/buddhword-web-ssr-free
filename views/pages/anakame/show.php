3740fd275d09fe833e18a547ba3a0c2e56e00087
souliya_pps <souliya.litd@gmail.com>
2026-06-15 16:34:08 +0700

    fix: store full route URL in Anakame favorite so list page links work correctly
    Previously stored just the raw href (e.g. '1000/1004.htm'), making
    favorites list page link to /1000/1004.htm instead of
    /anakame/read?href=1000%2F1004.htm

--- views/pages/anakame/show.php
+++ views/pages/anakame/show.php
@@ -145,7 +145,7 @@ class="relative min-h-screen pb-20">
                             isFavorite: (function() {
                                 try {
                                     var favs = JSON.parse(localStorage.getItem('buddhaword_favorites') || '[]');
-                                    var href = '<?= addslashes(preg_replace('/[[:cntrl:]]/', ' ', $href)) ?>';
+                                    var href = '<?= addslashes(preg_replace('/[[:cntrl:]]/', ' ', url('/anakame/read?href=' . urlencode($href)))) ?>';
                                     return href ? favs.some(function(f) { return f.href === href; }) : false;
                                 } catch(e) {
                                     return false;
@@ -154,7 +154,7 @@ class="relative min-h-screen pb-20">
                             toggleFavorite() {
                                 try {
                                     var favs = JSON.parse(localStorage.getItem('buddhaword_favorites') || '[]');
-                                    var item = { title: '<?= addslashes(preg_replace('/[[:cntrl:]]/', ' ', $title)) ?>', href: '<?= addslashes(preg_replace('/[[:cntrl:]]/', ' ', $href)) ?>' };
+                                    var item = { title: '<?= addslashes(preg_replace('/[[:cntrl:]]/', ' ', $title)) ?>', href: '<?= addslashes(preg_replace('/[[:cntrl:]]/', ' ', url('/anakame/read?href=' . urlencode($href)))) ?>' };
                                     if (this.isFavorite) {
                                         favs = favs.filter(function(f) { return f.href !== item.href; });
                                     } else {
